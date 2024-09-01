12183e8221ce3a773df9b5017c9aa91408ad15bd
LinuxerCGN <107237932+Proxserver@users.noreply.github.com>
2024-09-01 14:50:24 +0200

    Update uptimekuma.sh

--- uptimekuma.sh
+++ uptimekuma.sh
@@ -9,7 +9,16 @@ systemctl start docker.service
 docker volume create uptime-kuma
 docker run -d --restart=always -p 3001:3001 -v uptime-kuma:/app/data --name uptime-kuma louislam/uptime-kuma:latest
 
+echo"
+______               _       ___      _                     
+|  ___|             | |     / _ \    | |                    
+| |_ __ _ _ __ _   _| | __ / /_\ \ __| | __ _ _ __ ___  ___ 
+|  _/ _` | '__| | | | |/ / |  _  |/ _` |/ _` | '_ ` _ \/ __|
+| || (_| | |  | |_| |   <  | | | | (_| | (_| | | | | | \__ \
+\_| \__,_|_|   \__,_|_|\_\ \_| |_/\__,_|\__,_|_| |_| |_|___/
+"                                                            
+                                                            
 echo "Uptime Kuma wurde erfolgreich installiert und gestartet!"
-echo "Viel Spaß beim Monitoren Patrick"
+echo "Viel Spaß beim Monitoren Faruk"
 
 echo "Uptime Kuma login: http://deineIP:3001"
