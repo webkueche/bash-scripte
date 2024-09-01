012094e9c420f1e1a868b4859be7399709bc6992
Patrick Adams <107237932+Proxserver@users.noreply.github.com>
2024-09-01 19:49:25 +0200

    Update Checkmk.sh

--- Checkmk.sh
+++ Checkmk.sh
@@ -8,6 +8,7 @@ curl -sSL https://get.docker.com/ | CHANNEL=stable sh
 systemctl start docker
 systemctl start docker.service
 docker container run -dit -p 8080:5000 -p 8000:8000 --tmpfs /opt/omd/sites/cmk/tmp:uid=1000,gid=1000 -v monitoring:/omd/sites --name monitoring -v /etc/localtime:/etc/localtime:ro --restart always checkmk/check-mk-raw:2.3.0-latest
+docker container logs monitoring
 
 echo "
    _____       .___                      
@@ -21,6 +22,7 @@ echo "www.proxserver.de"
 echo ""
 echo "Checkmk wurde erfolgreich installiert und gestartet!"
 echo "Checkmk login: http://localhost:8080"
-echo "Default Zugangsdaten Benutzer:cmkadmin PW: Findest du nach der eingabe von: docker container logs monitoring"
+echo ""
+echo "Zugangsdaten Benutzer:cmkadmin PW: Findest du etwas weiter oben"
 echo ""
 echo "Viel Spaß beim monitoren mit Checkmk"
