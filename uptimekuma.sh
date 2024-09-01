4485a192a5c2ead108ada3a00b31a94f68b8e176
LinuxerCGN <107237932+Proxserver@users.noreply.github.com>
2024-09-01 14:56:37 +0200

    Update uptimekuma.sh

--- uptimekuma.sh
+++ uptimekuma.sh
@@ -2,14 +2,14 @@
 
 apt update
 apt upgrade -y
-apt install curl -y
+apt install -y curl
 curl -sSL https://get.docker.com/ | CHANNEL=stable sh
 systemctl start docker
 systemctl start docker.service
 docker volume create uptime-kuma
 docker run -d --restart=always -p 3001:3001 -v uptime-kuma:/app/data --name uptime-kuma louislam/uptime-kuma:latest
 
-echo"
+echo "
 ______               _       ___      _                     
 |  ___|             | |     / _ \    | |                    
 | |_ __ _ _ __ _   _| | __ / /_\ \ __| | __ _ _ __ ___  ___ 
