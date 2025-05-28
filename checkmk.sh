9190784540d67f32c7b4696e100d0e04ddf62794
Adams <adams@webkueche.com>
2025-05-28 15:23:17 +0200

    Test-Push

---
+++ checkmk.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Checkmk Kuma installiert..."
+
+apt update
+apt upgrade -y
+apt install -y curl
+curl -sSL https://get.docker.com/ | CHANNEL=stable sh
+systemctl start docker
+systemctl start docker.service
+docker container run -dit -p 8080:5000 -p 8000:8000 --tmpfs /opt/omd/sites/cmk/tmp:uid=1000,gid=1000 -v monitoring:/omd/sites --name monitoring -v /etc/localtime:/etc/localtime:ro --restart always checkmk/check-mk-raw:2.3.0-latest
+docker container logs monitoring
+
+echo "
+   _____       .___                      
+  /  _  \    __| _/____    _____   ______
+ /  /_\  \  / __ |\__  \  /     \ /  ___/
+/    |    \/ /_/ | / __ \|  Y Y  \\___ \ 
+\____|__  /\____ |(____  /__|_|  /____  >
+        \/      \/     \/      \/     \/ 
+"                                
+echo "www.proxserver.de"
+echo ""
+echo "Checkmk wurde erfolgreich installiert und gestartet!"
+echo "Checkmk login: http://localhost:8080"
+echo ""
+echo "Benutzer: cmkadmin PW: Findest du nach eingabe des Befehls:"
+echo "docker container logs monitoring"
+echo ""
+echo "Viel Spaß beim monitoren mit Checkmk"
+
+# test Upload to git, this line can be deleted
